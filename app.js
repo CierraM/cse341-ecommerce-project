7cb596c99ad8c659656dbb842f65b99897852bbd
Cierra Morris <4cibe@MacBook-Pro-4.local>
2021-10-07 10:11:16 -0600

    cors stuff

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
@@ -13,6 +14,22 @@ const User = require('./models/user')
 const app = express();
 //change this to work with heroku
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: "https://cierram-cse341-ecommerce-app.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://read-write-user:SWQ5mep2ULQjqvOl@cluster0.3lib4.mongodb.net/shop?retryWrites=true&w=majority";
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -37,7 +54,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://read-write-user:SWQ5mep2ULQjqvOl@cluster0.3lib4.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
+mongoose.connect('mongodb+srv://read-write-user:SWQ5mep2ULQjqvOl@cluster0.3lib4.mongodb.net/shop?retryWrites=true&w=majority', options).then(result => {
   User.findOne().then(user => {
     if (!user) {
       const user = new User({
